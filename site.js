7c099c4553565f1b7c0e3ce5c5892710b6b2a260
Logan <meltonlogan@me.com>
2023-05-30 07:50:45 -0500

    added roundup form and fixed modal

--- site.js
+++ site.js
@@ -2,17 +2,20 @@
 // Get the modal that is currently display: none;
 var modal = document.getElementById("myModal");
 // Get the button that opens the modal
-var btn = document.getElementById("donateButton");
+// var btn = document.getElementById("donateButton");
 // Get the <span> element that closes the modal
 var closer = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal
-btn.onclick = function () {
+
+function showModal() {
+  console.log("boop");
   modal.style.display = "block";
-};
+}
+
 // When the user clicks on <span> (x), close the modal
-closer.onclick = function () {
+function closeModal() {
   modal.style.display = "none";
-};
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
@@ -20,22 +23,22 @@ window.onclick = function (event) {
   }
 };
 
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slideShow");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("slideShow");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   setTimeout(showSlides, 3000); // Change image every 2 seconds
+// }
 
 // CashApp
 let caBtn = document.getElementById("cashAppDonate");
